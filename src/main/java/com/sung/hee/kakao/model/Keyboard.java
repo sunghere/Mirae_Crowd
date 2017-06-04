0fc5a812576db2b47ee8007c73434f97b5f1249c
JY <JY@JY-COM>
2017-06-05 00:10:57 +0900

    kakao 연동중

---
+++ src/main/java/com/sung/hee/kakao/model/Keyboard.java
@@ -0,0 +1,29 @@
+package com.sung.hee.kakao.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Keyboard implements Serializable {
+	private String type;
+	private List<String> buttons= new ArrayList<>();
+	
+	public void addButtons(String msg) {
+		buttons.add(msg);
+	}
+	public List<String> getButtons() {
+		return buttons;
+	}
+	public void setButtons(List<String> buttons) {
+		this.buttons = buttons;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+
+}
